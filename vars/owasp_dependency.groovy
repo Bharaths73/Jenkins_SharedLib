0ddf396fde26ef0362e1af8bd08c4eada5f119c0
Bharath_S <bharath7sms@gmail.com>
2025-10-04 12:13:16 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,13 +1,6 @@
 def call(){
-  // dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  // dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
-                sh '''
-                dependency-check.sh \
-                    --data /var/lib/jenkins/owasp-data \
-                    --project "Chat-App" \
-                    --scan . \
-                    --format "ALL" \
-                    --nvdApiKey ${NVD_API_KEY} \
-                    --out dependency-check-report
-                '''
+    dependencyCheck additionalArguments: "--scan ./ --format ALL --nvdApiKey ${NVD_API_KEY}",
+                                odcInstallation: 'OWASP',
+                                outdir: 'dependency-check-report'
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
