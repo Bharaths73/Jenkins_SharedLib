63e3fc4dbbc9343304461516723a191b76e103d4
Bharath_S <bharath7sms@gmail.com>
2025-10-05 13:59:53 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,5 +1,5 @@
 def call(){
-    dependencyCheck additionalArguments: "--scan ./ --format ALL --nvdApiKey ${NVD_API_KEY}",
+    dependencyCheck additionalArguments: "--scan ./ --format ALL --out ./dependency-check-report --nvdApiKey ${NVD_API_KEY}",
                                 odcInstallation: 'OWASP'
     dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
